dc295f421224ff631518979ed97465479608a851
unknown <jeronimodedeus@gmail.com>
2022-11-11 18:18:31 -0300

    Criando os relacionamentos do BD

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    funcionario = models.ForeignKey(Funcionario, models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.descricao
