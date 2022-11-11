924d6bb31f529304f94eaa58a2ba9ece7e981120
unknown <jeronimodedeus@gmail.com>
2022-11-11 17:43:03 -0300

    Criando a app Funcionarios

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text="Cadastro de Empresas")
+
+    def __str__(self):
+        return self.nome
