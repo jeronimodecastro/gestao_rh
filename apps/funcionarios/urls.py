5e4472ec8fb7b0be4b78a8a18038c1a077d4c6ca
unknown <jeronimodedeus@gmail.com>
2022-11-16 17:42:31 -0300

    Novas funcionalidades app Funcionarios Relatórios e Exportação

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -3,13 +3,18 @@ from .views import (
     FuncionariosCreateView,
     FuncionariosListView,
     FuncionariosUpdateView,
-    FuncionariosDeleteView,
+    FuncionariosDeleteView
 )
-
+from .views import pdf_reportlab, PdfHtml2Pdf, PdfHtml2PdfDebugView, ExpCsvView, ExpXlsView
 
 urlpatterns = [
     path('', FuncionariosListView.as_view(), name='list_funcionarios'),
     path('editar/<int:pk>/', FuncionariosUpdateView.as_view(), name='update_funcionario'),
     path('deletar/<int:pk>/', FuncionariosDeleteView.as_view(), name='delete_funcionario'),
     path('novo/', FuncionariosCreateView.as_view(), name='create_funcionario'),
+    path('pdf-reportlab/', pdf_reportlab, name='pdf_reportlab'),
+    path('pdf-html2pdf/', PdfHtml2Pdf.as_view(), name='pdf_html2pdf'),
+    path('pdf-html2pdf-debuf/', PdfHtml2PdfDebugView.as_view(), name='pdf_html2pdf_debug'),
+    path('exp-csv', ExpCsvView.as_view(), name='exp_csv'),
+    path('exp-xls', ExpXlsView.as_view(), name='exp_xls'),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,11 +1,21 @@
+import csv
+import io
+
+import xlwt
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import (
     ListView,
     UpdateView,
     DeleteView,
-    CreateView
+    CreateView,
+    TemplateView
 )
+from reportlab.pdfgen import canvas
+from django.template.loader import get_template
+import xhtml2pdf.pisa as pisa
 from .models import Funcionario
 
 
@@ -37,3 +47,109 @@ class FuncionariosCreateView(CreateView):
         funcionario.usuario = User.objects.create(username=funcionario.nome)
         funcionario.save()
         return super(FuncionariosCreateView, self).form_valid(form)
+
+
+def pdf_reportlab(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachament; filename="rel_funcionarios.pdf"'  # serve para baixar o arquivo
+
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+
+    p.drawString(200, 810, 'Relatório de Funcionários')
+
+    funcionarios = Funcionario.objects.filter(empresa=request.user.funcionario.empresa)
+
+    campos = 'Nome: %s | Hora Extra %.2f'
+
+    y = 750
+    for funcionario in funcionarios:
+        p.drawString(10, y, campos % (funcionario.nome, funcionario.total_horas_extras))
+        y -= 20
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
+
+
+class Render:
+    @staticmethod
+    def render(path: str, params: dict, filename: str):
+        template = get_template(path)
+        html = template.render(params)
+        response = io.BytesIO()
+        pdf = pisa.pisaDocument(
+            io.BytesIO(html.encode("UTF-8")), response)
+        if not pdf.err:
+            response = HttpResponse(response.getvalue(), content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename=%s.pdf' % filename
+            return response
+        else:
+            return HttpResponse("Erro renderizando PDF", status=400)
+
+
+class PdfHtml2Pdf(View):
+    def get(self, request):
+        funcionarios = Funcionario.objects.filter(empresa=request.user.funcionario.empresa)
+
+        params = {
+            'data': funcionarios,
+            'request': request,
+        }
+        return Render.render('funcionarios/relatorio.html', params, 'rel_funcionarios')
+
+
+class PdfHtml2PdfDebugView(TemplateView):
+    template_name = 'funcionarios/relatorio.html'
+
+
+class ExpCsvView(View):
+    def get(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=funcionarios.csv'
+
+        funcionarios = Funcionario.objects.filter(empresa=request.user.funcionario.empresa)
+
+        writer = csv.writer(response)
+        writer.writerow(['Nome', 'Total Horas'])
+        for funcionario in funcionarios:
+            writer.writerow([funcionario.nome, funcionario.total_horas_extras])
+        return response
+
+
+class ExpXlsView(View):
+    def get(self, request):
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename=funcionarios.xls'
+
+        wb = xlwt.Workbook(encoding='utf-8')
+        ws = wb.add_sheet('Funcionarios', cell_overwrite_ok=True)
+
+        row_num = 0
+
+        font_style = xlwt.XFStyle()
+        font_style.font.bold = True
+
+        colums = ['id', 'nome', 'Total de Horas']
+
+        for col_num in range(len(colums)):
+            ws.write(row_num, col_num, colums[col_num], font_style)
+
+        font_style = xlwt.XFStyle()
+
+        funcionarios = Funcionario.objects.filter(empresa=request.user.funcionario.empresa)
+
+        row_num = 1
+        for funcionario in funcionarios:
+            ws.write(row_num, 0, funcionario.id, font_style)
+            ws.write(row_num, 1, funcionario.nome, font_style)
+            ws.write(row_num, 2, funcionario.total_horas_extras, font_style)
+            row_num += 1
+        wb.save(response)
+        return response
+
