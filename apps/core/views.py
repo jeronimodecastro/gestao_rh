498f7f88c76fcd5e8611eefb56d9e70bd70d7063
unknown <jeronimodedeus@gmail.com>
2022-11-18 11:00:24 -0300

    Atualizando Templates

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 from django.shortcuts import render
-from apps.funcionarios.models import Funcionario
-
+from apps.horas_extras.models import HoraExtra
 
 @login_required
 def home(request):
     data = {}
     data['usuario'] = request.user
+    funcionario = request.user.funcionario
+    data['total_funcionarios'] = funcionario.empresa.total_funcionarios
+    data['total_funcionarios_ferias'] = funcionario.empresa.total_funcionarios_ferias
+    data['total_funcionarios_doc_pendente'] = funcionario.empresa.total_funcionarios_doc_pendente
+    data['total_funcionarios_doc_ok'] = funcionario.empresa.total_funcionarios_doc_ok
+    data['total_funcionarios_doc_rg'] = 5
+    data['total_he_pendente'] = HoraExtra.objects.filter(
+        funcionario__empresa=funcionario.empresa, utilizada=False).aggregate(
+        Sum('horas'))['horas__sum'] or 0
+    data['total_he_utilizada'] = HoraExtra.objects.filter(
+        funcionario__empresa=funcionario.empresa, utilizada=True).aggregate(
+        Sum('horas'))['horas__sum'] or 0
     return render(request, 'core/index.html', data)
--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -5,6 +5,25 @@ from django.urls import reverse
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text="Cadastro de Empresas")
 
+    @property
+    def total_funcionarios(self):
+        return self.funcionario_set.all().count()
+
+    @property
+    def total_funcionarios_ferias(self):
+        return self.funcionario_set.filter(de_ferias=True).count()
+
+    @property
+    def total_funcionarios_doc_pendente(self):
+        return self.funcionario_set.filter(documento=None).count()
+
+    @property
+    def total_funcionarios_doc_ok(self):
+        from django.db.models import Q
+        # ~Q vai pegar os funcionarios que possui documentos
+        return self.funcionario_set.filter(~Q(documento=None)).count()
+
+
     def __str__(self):
         return self.nome
 
